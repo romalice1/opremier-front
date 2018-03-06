9687d930fb44200926b8a329e45140505312a8b6
romalice1 <rommy_91@hotmail.fr>
2018-03-06 17:29:49 +0200

    Menu sliding completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from './services/user.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,19 @@ import { UserService } from './services/user.service';
 export class AppComponent {
   	title = 'Welcome to O-premier';
 
-	constructor(private user: UserService) { }
+	constructor(private user: UserService, private router: Router, private location: Location) {
+		//track router
+		router.events.subscribe((val)=>{
+			if(location.path() == '/stock'){
+				//Check if session is on
+				if( this.user.getUserLoggedIn() ){
+		        	this.isLoggedIn = true;
+				}
+		    }
+		});
+	}
 
+	
 	isLoggedIn = this.user.getUserLoggedIn();
 
 }
